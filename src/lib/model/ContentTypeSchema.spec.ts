3e38b379a73130bb3eae3f2d8de2e8ce1e25240e
Marc Easen <measen@amplience.com>
2019-09-24 14:32:14 +0100

    feat(content-type-schema): ability to create, update, get, get by version & list content-type-schemas

---
+++ src/lib/model/ContentTypeSchema.spec.ts
@@ -0,0 +1,59 @@
+import test from 'ava';
+import { MockDynamicContent } from '../DynamicContent.mocks';
+import { ContentItem } from './ContentItem';
+import { ContentTypeSchema } from './ContentTypeSchema';
+
+test('list ContentTypeSchemas for a Hub', async t => {
+  const client = new MockDynamicContent();
+  const hub = await client.hubs.get('5b32377e4cedfd01c45036d8');
+  const result = await hub.related.contentTypeSchema.list();
+  t.is(
+    result.getItems()[0].schemaId,
+    'http://example.com/content-type-schema.json'
+  );
+});
+
+test('get a ContentTypeSchema', async t => {
+  const client = new MockDynamicContent();
+  const result = await client.contentTypeSchemas.get(
+    '5d4af55ced6688002869d808'
+  );
+  t.is(result.schemaId, 'http://example.com/content-type-schema.json');
+});
+
+test('get a version ContentTypeSchema', async t => {
+  const client = new MockDynamicContent();
+  const result = await client.contentTypeSchemas.getByVersion(
+    '5d4af55ced6688002869d808',
+    2
+  );
+  t.is(result.schemaId, 'http://example.com/content-type-schema.json');
+});
+
+test('create a ContentTypeSchema', async t => {
+  const client = new MockDynamicContent();
+  const hub = await client.hubs.get('5b32377e4cedfd01c45036d8');
+  const newContentTypeSchema = new ContentTypeSchema();
+  newContentTypeSchema.schemaId = 'http://example.com/content-type-schema.json';
+  newContentTypeSchema.body = JSON.stringify({
+    $schema: 'http://json-schema.org/draft-04/schema#',
+    id: 'http://example.com/content-type-schema.json',
+    title: 'Image'
+  });
+  const createContentTypeSchema = await hub.related.contentTypeSchema.create(
+    newContentTypeSchema
+  );
+  t.is(
+    createContentTypeSchema.schemaId,
+    'http://example.com/content-type-schema.json'
+  );
+});
+
+test('update a ContentTypeSchema', async t => {
+  const client = new MockDynamicContent();
+  const result = await client.contentTypeSchemas.get(
+    '5d4af55ced6688002869d808'
+  );
+  const createContentTypeSchema = await result.related.update(result);
+  t.is(createContentTypeSchema.version, 2);
+});
---
+++ src/lib/model/ContentTypeSchema.ts
@@ -0,0 +1,83 @@
+import { HalResource } from '../hal/models/HalResource';
+import { Page } from './Page';
+
+/**
+ * Supported validation levels
+ */
+export enum ValidationLevel {
+  SLOT = 'SLOT',
+  CONTENT_TYPE = 'CONTENT_TYPE',
+  PARTIAL = 'PARTIAL'
+}
+
+/**
+ * Content type schema
+ */
+export class ContentTypeSchema extends HalResource {
+  /**
+   * Unique id generated on creation
+   */
+  public id?: string;
+
+  /**
+   * Content Type Schema
+   */
+  public body?: string;
+
+  /**
+   * Schema ID (URI)
+   */
+  public schemaId?: string;
+
+  /**
+   * Version number of the content type schema returned.
+   * This number will automatically be incremented for each update.
+   */
+  public version?: number;
+
+  /**
+   * Validation level for this content type schema
+   */
+  public validationLevel?: ValidationLevel;
+
+  /**
+   * Id of the user responsible for originally creating the content item
+   */
+  public createdBy?: string;
+
+  /**
+   * Timestamp representing when the content item was originally created in ISO 8601 format
+   */
+  public createdDate?: string;
+
+  /**
+   * Id of the user responsible for the last update to the content item
+   */
+  public lastModifiedBy?: string;
+
+  /**
+   * Timestamp representing when the content item was last updated in ISO 8601 format
+   */
+  public lastModifiedDate?: string;
+
+  /**
+   * Resources and actions related to a ContentTypeSchema
+   */
+  public readonly related = {
+    /**
+     * Updated content type schema
+     * @param updated
+     */
+    update: (mutation: ContentTypeSchema): Promise<ContentTypeSchema> =>
+      this.updateResource(mutation, ContentTypeSchema)
+  };
+}
+
+/**
+ * @hidden
+ */
+export class ContentTypeSchemaPage extends Page<ContentTypeSchema> {
+  constructor(data?: any) {
+    super('content-type-schemas', ContentTypeSchema, data);
+  }
+}
