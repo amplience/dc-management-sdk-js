d05e53c51186bc06a2263a6ef453c64f8fbac493
Marc Easen <measen@amplience.com>
2020-08-12 09:41:48 +0100

    feat(search indexes): include replica options

--- src/lib/model/SearchIndex.spec.ts
+++ src/lib/model/SearchIndex.spec.ts
@@ -2,6 +2,7 @@ import test from 'ava';
 import { MockDynamicContent } from '../DynamicContent.mocks';
 import { AssignedContentType } from './AssignedContentType';
 import { SearchIndex } from './SearchIndex';
+import { SearchesOrderBy } from './SearchIndexTopSearches';
 
 test('get search index by id', async (t) => {
   const client = new MockDynamicContent();
@@ -143,7 +144,7 @@ test('delete an index object', async (t) => {
   );
 });
 
-test('get top-results analytics for a search index', async (t) => {
+test('get top-results analytics for a search index should default to clickAnalytics=false', async (t) => {
   const client = new MockDynamicContent();
   const hub = await client.hubs.get('5b32377e4cedfd01c45036d8');
   const searchIndex = await hub.related.searchIndexes.get(
@@ -154,27 +155,68 @@ test('get top-results analytics for a search index', async (t) => {
   t.is(item.search, 'q0');
   t.is(item.nbHits, 1);
   t.is(item.count, 1);
+  const getRequests = client.mock.history['get'];
+  const query = getRequests[getRequests.length - 1].url.split('?')[1];
+  t.is(query, 'clickAnalytics=false');
 });
 
-test('get top-hits analytics for a search index', async (t) => {
+test('get top-results analytics for a search index', async (t) => {
   const client = new MockDynamicContent();
   const hub = await client.hubs.get('5b32377e4cedfd01c45036d8');
   const searchIndex = await hub.related.searchIndexes.get(
     '00112233445566778899aabb'
   );
-  const result = await searchIndex.related['top-hits'].get({});
+  const result = await searchIndex.related['top-searches'].get({
+    clickAnalytics: true,
+    direction: 'asc',
+    endDate: '2020-12-31',
+    startDate: '2020-01-01',
+    includeReplicas: true,
+    limit: 10,
+    offset: 20,
+    orderBy: SearchesOrderBy.SEARCH_COUNT,
+    tags: 'additional_tags',
+  });
+  const item = result.getItems()[0];
+  t.is(item.search, 'q0');
+  t.is(item.nbHits, 1);
+  t.is(item.count, 1);
+});
+
+test('get top-hits analytics for a search index with no search term', async (t) => {
+  const client = new MockDynamicContent();
+  const hub = await client.hubs.get('5b32377e4cedfd01c45036d8');
+  const searchIndex = await hub.related.searchIndexes.get(
+    '00112233445566778899aabb'
+  );
+  const result = await searchIndex.related['top-hits'].get({
+    endDate: '2020-12-31',
+    startDate: '2020-01-01',
+    includeReplicas: true,
+    limit: 10,
+    offset: 20,
+    tags: 'additional_tags',
+  });
   const item = result.getItems()[0];
   t.is(item.count, 123);
   t.is(item.hit, 'ObjectID');
 });
 
-test('get top-hits analytics for a search index for a given search term', async (t) => {
+test('get top-hits analytics for a search index for a search term', async (t) => {
   const client = new MockDynamicContent();
   const hub = await client.hubs.get('5b32377e4cedfd01c45036d8');
   const searchIndex = await hub.related.searchIndexes.get(
     '00112233445566778899aabb'
   );
-  const result = await searchIndex.related['top-hits'].get({ search: 'term' });
+  const result = await searchIndex.related['top-hits'].get({
+    search: 'term',
+    endDate: '2020-12-31',
+    startDate: '2020-01-01',
+    includeReplicas: true,
+    limit: 10,
+    offset: 20,
+    tags: 'additional_tags',
+  });
   const item = result.getItems()[0];
   t.is(item.count, 123);
   t.is(item.hit, 'ObjectID');
