9dd4a827bc00401daae8c7cdd2a30d0a143a1654
Rhys <rsimpson@amplience.com>
2021-09-15 09:53:43 +0100

    docs: update docs for assigned content type

--- src/lib/model/AssignedContentType.ts
+++ src/lib/model/AssignedContentType.ts
@@ -26,21 +26,39 @@ export class AssignedContentType extends HalResource {
    */
   public lastModifiedDate?: string;
 
+  /**
+   * Resources and actions related to an Assigned Content Type.
+   */
   public readonly related = {
+    /**
+     * Unassigns the content type from the search index.
+     */
     unassign: (id: string): Promise<void> =>
       this.deleteLinkedResource('unassign', {
         id,
       }),
 
+    /**
+     * Recreates the webhooks for this Assigned Content Type.
+     */
     recreateWebhook: (): Promise<void> =>
       this.performActionWithoutResourceResponse('recreate-webhook'),
 
+    /**
+     * Gets the primary webhook for this Assigned Content Type.
+     */
     webhook: (): Promise<Webhook> =>
       this.fetchLinkedResource('webhook', {}, Webhook),
 
+    /**
+     * Gets the active content webhook for this Assigned Content Type.
+     */
     activeContentWebhook: (): Promise<Webhook> =>
       this.fetchLinkedResource('active-content-webhook', {}, Webhook),
 
+    /**
+     * Gets the archived content webhook for this Assigned Content Type.
+     */
     archivedContentWebhook: (): Promise<Webhook> =>
       this.fetchLinkedResource('archived-content-webhook', {}, Webhook),
   };
