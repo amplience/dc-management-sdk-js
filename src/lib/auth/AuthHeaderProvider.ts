e96e18061a035260643ca3dcb564a326638304d4
Sam Johnson <86957918+samjohnsonamp@users.noreply.github.com>
2024-01-19 14:38:12 +0000

    feat: add comments

--- src/lib/auth/AuthHeaderProvider.ts
+++ src/lib/auth/AuthHeaderProvider.ts
@@ -1,3 +1,8 @@
+/**
+ * AuthHeaderProvider is used to provide a getToken method to provide and authorization token
+ * Oauth2Client and PatTokenClient are two options for providing an AuthHeaderProvider
+ */
+
 export interface AuthHeaderProvider<T> {
   getToken(): Promise<T>;
 }
